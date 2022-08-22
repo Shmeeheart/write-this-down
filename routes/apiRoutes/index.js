a31aca20000ee3cc3188dc4785cff5c8d842b053
Jennifer <goodwije@gmail.com>
2022-08-21 19:46:27 -0600

    trying to fix stuff

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const notesRoutes = require('../apiRoutes/notesRoutes');
+const notesRoutes = require('../apiRoutes/notes');
 
 router.use(notesRoutes);
 
--- server.js
+++ server.js
@@ -16,5 +16,3 @@ app.use('/', htmlRoutes);
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
-
-hello();
